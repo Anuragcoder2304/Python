a704ad75b4e4e2b76488901e709065daedb13408
Anuragcoder2304 <singhanurag3111@gmail.com>
2026-07-04 13:09:50 +0530

    pyhton code

---
+++ 01_Python_output.py
@@ -0,0 +1,82 @@
+#Python output
+
+print("Hello world")
+print("I am anurag ")
+print ("My age is 20")
+print("I am anurag ","My age is 20")
+print(34)
+print(34+345)
+
+print(1,2,3,4,5, sep = '/')
+print("hello")
+print("World")
+print("Hello", end='-')
+print("world")
+
+name = "Dev" 
+last_name = "ali"
+print(name +" "+ last_name)
+len(last_name)
+long = "_" * 10
+print(long)
+print(len(long))
+
+#Data type
+# Integer
+print(8)
+# 1*10^308
+print(1e308)
+print(1e309)
+
+# Decimal/Float
+print(8.55)
+print(1.7e309)
+
+# Boolean
+print(True)
+print(False)
+
+# Text/String
+print('Hello World')
+
+# complex
+print(5+6j)
+
+
+name= "Anurag"
+age= 20
+price= 24.66
+age2=age+5
+
+print(type(name))
+print(type(age))
+print(type(price))
+
+old=False
+a=None
+
+print(type(old))
+print(type(a))
+
+a=3
+b=4
+sum=a+b
+print(a+b)
+print(sum)
+
+
+# List-> C-> Array
+print([1,2,3,4,5])
+
+# Tuple
+print((1,2,3,4,5))
+
+# Sets
+print({1,2,3,4,5})
+
+# Dictionary
+print({'name':'Nitish','gender':'Male','weight':70})
+
+# type
+print(type([1,2,3]))
+

30ccc4d20fe5c6d61ce1c3e6ccdbe132e490022d
Anuragcoder2304 <singhanurag3111@gmail.com>
2026-07-05 21:36:26 +0530

    Second code

---
+++ 02_variables.py
@@ -0,0 +1,64 @@
+# Static Vs Dynamic Typing
+# Static Vs Dynamic Binding
+# stylish declaration techniques
+
+name ='Raj'
+print(name)
+
+a=5
+b=6
+print(a + b)
+
+
+# Dynamic Typing
+a = 5
+# Static Typing
+#int a = 5
+
+#Dynamic Binding 
+a =5
+print(a)
+a = 'nitish'
+print(a)
+
+#Static Binding
+# int a =5
+
+
+a=1
+b=2
+c=3
+print(a,b,c)
+
+a,b,c = 4,5,6
+print(a,b,c)
+
+a=b=c=7
+print(a,b,c)
+
+
+
+##comments##
+#This line is a comment
+#Second line
+'''multiple line
+can be comment
+using this 
+command'''
+"""
+this commond is 
+also use for
+comment 
+of multiple line."""
+
+
+
+## Keywords & Identifiers ##
+# Identifiers
+# You can't start with a digit
+name1 = 'Nitish'
+print(name1)
+# You can use special chars -> _
+_ = 'ntiish'
+print(_)
+# identiers can not be keyword
